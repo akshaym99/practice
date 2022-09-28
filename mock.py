aeb481677471a319392e6468690d574804911d70
akshaym99 <92678608+akshaym99@users.noreply.github.com>
2022-09-28 15:20:59 +0530

    Update mock.py

--- mock.py
+++ mock.py
@@ -1,3 +1,26 @@
+'''
+------------------------ Problem 1 Start ---------------------------
+Input:
+- Credit card transactions data with possible duplicates
+- System is getting one transaction record at a time, unordered
+- TransactionID, TransactionTime, <other transactional information>
+
+Output:
+- Print whether UNIQUE or DUPLICATE record.
+
+Purge policy:
+- To reduce memory requirements, you need to purge data older than 1 hour.
+- The purge function would be invoked once a  minute.
+- Purge is based on transactionTime
+
+Guidelines:
+- For simplicity, use in-memory data structures to solve this problem, no need to use external system 
+- Use programming language and editor of your choice
+- You can browse the web if you need any help
+
+------------------------ Problem 1 End ---------------------------
+'''
+
 class CC_Transaction:
 
     def __init__(self):
